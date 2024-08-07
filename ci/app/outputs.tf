94be6a8c4423eff4a4b5cf24265a3caf16a737b2
john@ap-mic.com <john@ap-mic.com>
2024-08-07 06:17:42 +0000

    ci: pwd is sensitive

--- ci/app/outputs.tf
+++ ci/app/outputs.tf
@@ -1,7 +1,4 @@
 output "pwd" {
-  value = nonsensitive(random_password.vm.result)
-}
-
-output "ip" {
-  value = azurerm_public_ip.app.ip_address
+ value     = random_password.vm.result
+ sensitive = true
 }
